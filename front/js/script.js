243a0e52d161780767bf501f8744033cb88b3754
mohamadmahdi <mohamadmahditajkarimi@gmail.com>
2024-01-29 17:04:34 +0330

    gallery

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,54 @@
 $(document).ready(function () {
+    let currentImageIndex = 0;
+    const images = ["/img/seyedGreenBack.jpg", "/img/inpeople.jpg", "/img/next-to-flag.jpg", "/img/whenSpeching.jpg"];
+
+    function openModal(imageSrc) {
+        const modal = $("#myModal");
+        const modalImg = $("#modalImg");
+
+        modal.css("display", "flex");
+        modalImg.attr("src", imageSrc);
+        currentImageIndex = images.indexOf(imageSrc);
+    }
+
+    function closeModal() {
+        $("#myModal").css("display", "none");
+    }
+
+    function nextImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        $("#modalImg").attr("src", images[currentImageIndex]);
+    }
+
+    function prevImage() {
+        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+        $("#modalImg").attr("src", images[currentImageIndex]);
+    }
+
+    $(document).ready(function () {
+        $("#closeModalBtn, #closeBtn").click(function () {
+            closeModal();
+        });
+
+        $(window).click(function (event) {
+            if ($(event.target).is("#myModal")) {
+                closeModal();
+            }
+        });
+
+        $("#prevBtn").click(function () {
+            prevImage();
+        });
+
+        $("#nextBtn").click(function () {
+            nextImage();
+        });
+
+        $(".cursor-pointer").click(function () {
+            openModal($(this).find("img").attr("src"));
+        });
+    });
+
     var backButton = $("#backButton");
 
     $(window).scroll(function () {
@@ -41,19 +91,6 @@ $(document).ready(function () {
         }, 2000);
     });
 
-    let prevScrollpos = window.pageYOffset;
-
-    $(window).scroll(function () {
-        let currentScrollPos = window.pageYOffset;
-
-        if (prevScrollpos > currentScrollPos) {
-            $("#header").css("transform", "translateY(0)");
-        } else {
-            $("#header").css("transform", "translateY(-100%)");
-        }
-
-        prevScrollpos = currentScrollPos;
-    });
     $(document).ready(function () {
         $("#newsDropdown").addClass("hidden");
 
@@ -112,4 +149,65 @@ $(document).ready(function () {
     setTimeout(function () {
         $("#myGallery3").removeClass("opacity-0").addClass("opacity-100", "");
     }, 600);
+});
+function imagesGallery() {
+    return {
+        isOpen: false,
+        currentImage: 0,
+        images: ["/img/whenSpeching.jpg", "/img/inpeople.jpg", "/img/next-to-flag.jpg", "/img/seyedGreenBack.jpg"],
+        openModal(index) {
+            this.currentImage = index;
+            this.isOpen = true;
+        },
+        closeModal() {
+            this.isOpen = false;
+        },
+        nextImage() {
+            this.currentImage = Math.min(this.currentImage + 1, this.images.length - 1);
+        },
+        prevImage() {
+            this.currentImage = Math.max(this.currentImage - 1, 0);
+        },
+    };
+}
+$(document).ready(function () {
+    let prevScrollpos = window.pageYOffset;
+    let isImageOpen = false;
+
+    // Function to handle image opening
+    function openImage() {
+        isImageOpen = true;
+        $("#header").addClass("hidden");
+    }
+
+    // Function to handle image closing
+    function closeImage() {
+        isImageOpen = false;
+        $("#header").removeClass("hidden");
+    }
+
+    // Set up scroll event listener
+    $(window).scroll(function () {
+        let currentScrollPos = window.pageYOffset;
+
+        // Check if an image is open
+        if (!isImageOpen) {
+            if (prevScrollpos > currentScrollPos) {
+                $("#header").css("transform", "translateY(0)");
+            } else {
+                $("#header").css("transform", "translateY(-100%)");
+            }
+        }
+
+        prevScrollpos = currentScrollPos;
+    });
+
+    // Example usage when opening/closing an image
+    $("#openImageBtn").click(function () {
+        openImage();
+    });
+
+    $("#closeImageBtn").click(function () {
+        closeImage();
+    });
 });
